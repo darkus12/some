a267b90a07379fe1f01e064c02b484db200c776c
Stas <mr.darkusmen@gmail.com>
2025-08-10 12:52:29 +0300

    хз

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            //36 сторінка 24 завдання
+            Console.WriteLine(1+0.0);
+            Console.WriteLine(2 - 3);
+            Console.WriteLine(Math.Sin(0));
+            Console.WriteLine(2*2);
+            Console.WriteLine(1%2);
+            Console.WriteLine(2 * 5);
+            Console.WriteLine(Math.Sqrt(4));
+            
+            
+
+        }
+    }
+}
